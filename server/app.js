12326283f4952f84a3e882a9e921d2fba11b2236
Lex Aeterna <lexaeterna12321@gmail.com>
2019-10-20 15:13:00 +0200

    established Db connection

--- server/app.js
+++ server/app.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const mongoose = require("mongoose");
 const schema = require("./schema/schema");
+require("dotenv").config();
 
+const { DB_USER, DB_PASS } = process.env;
+
+mongoose
+  .connect(
+    `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0-3vzik.mongodb.net/test?retryWrites=true&w=majority`,
+    { useNewUrlParser: true }
+  )
+  .catch(err => console.log(`Error in Db connection: ${err.message}`));
+
+mongoose.connection.once("open", () => console.log("Connected to Database"));
 const app = express();
 
 app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
